4b5765823a53e83f22423f90f355c0ad16910d41
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2023-01-18 23:59:45 +0300

    add useState, input ,button.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import {useState} from "react" 
+import {add} from "./features/todoSlice"
+import { useAppDispatch, useAppSelector } from "./store"
 
 function App() {
+  const todos = useAppSelector(state =>state.todos)
+  const [title,setTitle] = useState("")
+
+  const dispatch =useAppDispatch()
+
+  const onSave=() => {
+    dispatch (add(title))
+    setTitle("")
+  }
+
   return (
     <div className="App">
-    
+    <input name="title" value={title} onChange={(e) => setTitle(e.currentTarget.value)} />
+    <button onClick={onSave}>Save</button>
+    <ul>
+      {todos.map(todo => <li key={todo.id}>{todo.title}</li>)}
+    </ul>
     </div>
   );
 }
