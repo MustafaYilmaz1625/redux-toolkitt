963abe208e446d3b06b2e3f0422e1955a6661096
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2023-01-19 01:31:22 +0300

    add userSlice

---
+++ src/features/userSlice.ts
@@ -0,0 +1,110 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface UserState {
+    data:User | null,
+    loading:boolean,
+    error:string 
+}
+
+const initialState:UserState ={
+    data:null,
+    loading:false,
+    error:""
+}
+
+const userSlice =createSlice({
+    name: "user",
+    initialState,
+    reducers:{},
+    extraReducers:()=>{},
+})
+
+
+ export interface Name {
+   title: string;
+   first: string;
+   last: string;
+ }
+
+ export interface Street {
+   number: number;
+   name: string;
+ }
+
+ export interface Coordinates {
+   latitude: string;
+   longitude: string;
+ }
+
+ export interface Timezone {
+   offset: string;
+   description: string;
+ }
+
+ export interface Location {
+   street: Street;
+   city: string;
+   state: string;
+   country: string;
+   postcode: string;
+   coordinates: Coordinates;
+   timezone: Timezone;
+ }
+
+ export interface Login {
+   uuid: string;
+   username: string;
+   password: string;
+   salt: string;
+   md5: string;
+   sha1: string;
+   sha256: string;
+ }
+
+ export interface Dob {
+   date: Date;
+   age: number;
+ }
+
+ export interface Registered {
+   date: Date;
+   age: number;
+ }
+
+ export interface Id {
+   name: string;
+   value: string;
+ }
+
+ export interface Picture {
+   large: string;
+   medium: string;
+   thumbnail: string;
+ }
+
+ export interface Result {
+   gender: string;
+   name: Name;
+   location: Location;
+   email: string;
+   login: Login;
+   dob: Dob;
+   registered: Registered;
+   phone: string;
+   cell: string;
+   id: Id;
+   picture: Picture;
+   nat: string;
+ }
+
+ export interface Info {
+   seed: string;
+   results: number;
+   page: number;
+   version: string;
+ }
+
+ export interface User {
+   results: Result[];
+   info: Info;
+ }
