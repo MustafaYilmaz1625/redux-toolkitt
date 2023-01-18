8124ff79b074fab341ad8374f5ef69885311e451
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2023-01-19 00:00:40 +0300

    add uuid(v4)

--- src/features/todoSlice.ts
+++ src/features/todoSlice.ts
@@ -1,4 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import {v4} from "uuid";
 
 export interface Todo {
   id: string;
@@ -13,8 +14,15 @@ const todoSlice = createSlice({
   initialState,
   reducers: {
     add: (state, action: PayloadAction<string>) => {
-      const newTodo = { id: "", title: action.payload, completed: false };
+      const newTodo = {
+        id: v4(),
+        title: action.payload,
+        completed: false,
+      };
       state.push(newTodo);
     },
   },
 });
+
+export default todoSlice.reducer;
+export const { add } = todoSlice.actions;
